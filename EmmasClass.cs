191fcf026f1e15bdcd8533b113a99459f5ffa555
EmmaHyttel <emh004@edu.zealand.dk>
2023-11-13 10:42:45 +0100

    Jeg har lavet en klasse EmmasClass

---
+++ EmmasClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML2_BigMamma
+{
+    public class EmmasClass
+    {
+        public string Besked { get; set; }
+
+        public EmmasClass(string besked)
+        {
+            Besked = besked;
+        }
+
+        public void SendBesked()
+        {
+            Console.WriteLine("Goddaw ladies <3");
+        }
+    }
+}
