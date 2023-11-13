f56dd33f56b4f4b01b0f7de0f5ced0668c2caeb3
Marjorine Pearl Zalwango <maz001@edu.zealand.dk>
2023-11-13 10:50:46 +0100

    Created a class

---
+++ Marjopclass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML2_BigMamma
+{
+    internal class Marjopclass
+    {
+    }
+}
